ec4ea3f440caf7f1a88dea38cecb8b97361e4813
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2025-05-26 13:55:19 +0200

    Basic home layout

--- SpotMyAlien/routes/web.php
+++ SpotMyAlien/routes/web.php
@@ -9,6 +9,8 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/contact', fn () => view('contact'))->name('contact');
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');

93b49bbb72e4948ee7be64b6705fc9f095b5aa15
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2025-05-26 14:23:06 +0200

    installed tailwind/vite

--- SpotMyAlien/vite.config.js
+++ SpotMyAlien/vite.config.js
@@ -4,14 +4,13 @@ import {
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        tailwindcss()
     ],
     server: {
         cors: true,

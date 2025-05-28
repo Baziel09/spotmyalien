32d14d3a5871fa2b37526bff3ad842aab6b044be
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2025-05-28 10:21:58 +0200

    - Same styling for buttons on hero banner - capital letters in header - removed 'logo' from footer - removed lorem ipsum

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,3 @@
 <footer class="bg-gray-900 text-white text-center py-4 text-sm">
-        <p>Logo</p>
         <p>Made by team Baziel, Karel, Lisa & Brent</p>
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,8 +3,10 @@
         <a href="{{ route('home') }}"><img src="{{ asset('images/alien-space.svg') }}" alt="{{ $logoAlt }}"></a>
     </div>
     <nav class="space-x-4">
-        <a href="{{ route('login') }}" class="text-sm hover:underline">login</a>
-        <a href="{{ route('register') }}" class="text-sm hover:underline">register</a>
+        @guest
+        <a href="{{ route('login') }}" class="text-sm hover:underline">Login</a>
+        <a href="{{ route('register') }}" class="text-sm hover:underline">Register</a>
+        @endguest
         <a href="{{ route('about') }}" class="text-sm hover:underline">Over ons</a>
         <a href="{{ route('contact') }}" class="text-sm hover:underline">Contact</a>
         @auth
